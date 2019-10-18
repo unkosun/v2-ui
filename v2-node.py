09eeaa269de434ea9bcbc29fa3c622d841f08e25
style <style@dom>
2019-10-18 22:09:28 +0800

    监听程序增加了打印日志，.service文件中增加了总是重启，保证应用因故障退出后仍能重新启动

--- v2-node.py
+++ v2-node.py
@@ -9,17 +9,18 @@
 from socket import *
 import json
 import struct
+import logging
 import subprocess
 
 
 def node_added(conn_socket):
-    print("[I] Handling node added...")
+    logging.debug("[I] Handling node added...")
     conn_socket.send("ack".encode("utf-8"))
 
 
 def config_changed(conn_socket, filesize):
-    print("[I] Handling config changed...")
-    print("[I] Ready to receive file with size: %d" % filesize)
+    logging.debug("[I] Handling config changed...")
+    logging.debug("[I] Ready to receive file with size: %d" % filesize)
     recv_len = 0
     with open("/etc/v2ray/config.json", "wb") as f:
         while recv_len < filesize:
@@ -27,25 +28,28 @@ def config_changed(conn_socket, filesize):
             # print(data)
             f.write(data)
             recv_len += len(data)
-    print("[I] File receiving done.")
-    print("[I] Restarting v2ray service...")
+    logging.debug("[I] File receiving done.")
+    logging.debug("[I] Restarting v2ray service...")
     code = -100
-    p = None
     try:
         p = subprocess.Popen("service v2ray restart", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         code = p.wait()
         if code != 0:
-            print(p.stdout.read().decode('utf-8'), code)
+            logging.debug(p.stdout.read().decode('utf-8'), code)
         result = p.stdout.read()
-        print("[I] %s" % result.decode('utf-8'))
-        print("[I] Successfully started.")
+        logging.debug("[I] %s" % result.decode('utf-8'))
+        logging.debug("[I] Successfully started.")
     except Exception as e:
-        print(str(e), code)
+        logging.error(str(e), code)
     finally:
-        print("[I] Done.")
+        logging.debug("[I] Done.")
 
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='/etc/v2-node/v2-node.log',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        format='%(asctime)s-%(name)s-%(levelname)s-%(message)s',
+                        level=logging.ERROR)
     svr = socket(AF_INET, SOCK_STREAM)
     try:
         svr.bind(("0.0.0.0", 40001))
@@ -53,14 +57,14 @@ if __name__ == "__main__":
         svr.setsockopt(SOL_SOCKET, SO_REUSEPORT)
         svr.bind(("0.0.0.0", 40001))
     svr.listen(5)
-    print("[I] Listening on 40001...")
+    logging.error("[I] Listening on 40001...")
     while True:
         try:
             conn, addr = svr.accept()
-            print("[I] Received connection from: %s:%d" % addr)
+            logging.debug("[I] Received connection from: %s:%d" % addr)
             header_len = conn.recv(4)
             if header_len:
-                print("[I] Ready to receive data.")
+                logging.debug("[I] Ready to receive data.")
             header_len = struct.unpack('i', header_len)[0]
             data = conn.recv(header_len).decode("utf-8")
             data = json.loads(data)
@@ -71,12 +75,12 @@ if __name__ == "__main__":
                 elif cmd == "config_changed":
                     config_changed(conn, data["filesize"])
                 else:
-                    print("[E] Unsupported command: %s." % cmd)
+                    logging.error("[E] Unsupported command: %s." % cmd)
             else:
-                print("[E] No data received.")
+                logging.error("[E] No data received.")
             conn.close()
-        except Exception as e:
-            print("[E] Catched exceptions: %s " % str(e))
-            continue
         except KeyboardInterrupt as e:
             break
+        except Exception as e:
+            logging.error("[E] Catches exceptions: %s " % str(e))
+            continue
